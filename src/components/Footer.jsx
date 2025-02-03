db1e645cade205209b1c064c7108abfc7dbe1fec
Vernere <Verner.etola@metropolia.fi>
2025-02-03 20:00:54 +0200

    Links fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,42 +4,6 @@ function Footer() {
   return (
     <footer className="section footer">
       <PageLinks parentClass='footer-links' itemClass='footer-link' />
-      <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-facebook" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-twitter" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.squarespace.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-squarespace" />
-          </a>
-        </li>
-      </ul>
-      <p className="copyright">
-        copyright © travel tours company<span id="date">2025</span>. all rights
-        reserved
-      </p>
     </footer>
 
   );
