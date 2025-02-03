58d1e13452c5581219fb622f5dd41961a7902cf9
Vernere <Verner.etola@metropolia.fi>
2025-02-03 19:21:57 +0200

    Working about page

--- src/components/CodeWrapper.jsx
+++ src/components/CodeWrapper.jsx
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { ghcolors } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { code } from '../data'; // Import the code from data.js
 import './CodeWrapper.css';
 
 const CodeWrapper = () => {
-  const [codeText, setCodeText] = useState('');
+    const [codeText, setCodeText] = useState('');
 
-  useEffect(() => {
-    // Add non-breaking spaces to the beginning of each line to push the text 15% to the right
-    const indentedCode = code[0].text.split('\n').map(line => '\u00A0'.repeat(15) + line).join('\n');
-    setCodeText(indentedCode); // Set the indented code text
-  }, []);
+    useEffect(() => {
+        // Add non-breaking spaces to the beginning of each line to push the text 15% to the right
+        const indentedCode = code[0].text.split('\n').map(line => '\u00A0'.repeat(15) + line).join('\n');
+        setCodeText(indentedCode); // Set the indented code text
+    }, []);
 
-  const formatLineNumber = (lineNumber) => {
-    return '\u00A0'.repeat(15) + lineNumber.toString().padStart(2, '0');
-  };
+    const formatLineNumber = (lineNumber) => {
+        return '\u00A0'.repeat(15) + lineNumber.toString().padStart(2, '0');
+    };
 
-  return (
-    <SyntaxHighlighter
-      language="python"
-      style={coy}
-      showLineNumbers
-      wrapLines
-      lineProps={(lineNumber) => ({
-        className: 'code-line',
-        style: { display: 'flex', width: '100%' },
-        children: (
-          <>
-            <span className="line-number">{formatLineNumber(lineNumber)}</span>
-            <span>{'\u00A0'.repeat(15)}{lineNumber.content}</span>
-          </>
-        ),
-      })}
-    >
-      {codeText}
-    </SyntaxHighlighter>
-  );
+    return (
+        <SyntaxHighlighter
+            language="python"
+            style={ghcolors}
+            showLineNumbers
+            wrapLines
+            lineProps={(lineNumber) => ({
+                className: 'code-line',
+                style: { display: 'flex', width: '100%' },
+                children: (
+                    <>
+                        <span className="line-number">{formatLineNumber(lineNumber)}</span>
+                        <span>{'\u00A0'.repeat(15)}{lineNumber.content}</span>
+                    </>
+                ),
+            })}
+        >
+            {codeText}
+        </SyntaxHighlighter>
+    );
 };
 
 export default CodeWrapper;
